7434e1ba63fa37d9c2cbdf28ce37480a65e83bea
Vincent Kempers <vincent.m.kempers@gmail.com>
2023-05-09 14:44:15 +0200

    adding base entity and actions

--- libs/core-data/src/lib/state/projects/projects.actions.ts
+++ libs/core-data/src/lib/state/projects/projects.actions.ts
@@ -4,14 +4,20 @@ import { Project } from './../../projects/project.model';
 
 export enum ProjectsActionTypes {
   ProjectSelected = '[Projects] Selected',
+  LoadProjects = '[Projects] Load Data',
   AddProject = '[Projects] Add Data',
   UpdateProject = '[Projects] Update Data',
   DeleteProject = '[Projects] Delete Data',
 }
 
+
 export class SelectProject implements Action {
   readonly type = ProjectsActionTypes.ProjectSelected;
   constructor(private payload: Project) {}
+}
+export class LoadProjects implements Action {
+  readonly type = ProjectsActionTypes.LoadProjects;
+  constructor(private payload: Project[]) {}
 }
 export class AddProject implements Action {
   readonly type = ProjectsActionTypes.AddProject;
@@ -28,6 +34,7 @@ export class DeleteProject implements Action {
 
 export type ProjectsActions = SelectProject
   | AddProject
+  | LoadProjects
   | UpdateProject
   | DeleteProject
   ;
